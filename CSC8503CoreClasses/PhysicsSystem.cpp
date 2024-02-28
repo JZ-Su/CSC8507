06fde147da72d7ceceb7c4b20851d150b7e374c1
Ayesha <111573017+ayeshatazeen1@users.noreply.github.com>
2024-02-28 11:31:18 +0000

    Linear damping
    Fixed player motion , player is not slipping on floor now.

--- CSC8503CoreClasses/PhysicsSystem.cpp
+++ CSC8503CoreClasses/PhysicsSystem.cpp
@@ -394,7 +394,7 @@ void PhysicsSystem::IntegrateVelocity(float dt) {
 	std::vector<GameObject*>::const_iterator first;
 	std::vector<GameObject*>::const_iterator last;
 	gameWorld.GetObjectIterators(first, last);
-	float frameLinearDamping = 1.0f - (0.4f * dt);
+	float frameLinearDamping = 1.0f - (0.85f * dt);
 
 	for (auto i = first; i != last; i++) {
 		PhysicsObject* object = (*i)->GetPhysicsObject();
